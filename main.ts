c3a355bb11349cfff9850ad147a66685a107f9f1
LennyBNF <132681048+LennyBNF@users.noreply.github.com>
2023-05-26 17:28:44 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,19 @@
+basic.forever(function () {
+    if (BitMaker.read_Din_value(GrovePort.P8) > 0) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # # # .
+            # . . . #
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
+    }
+})
