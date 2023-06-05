01ad1936803b312289d6419cf955192016ac1516
LennyBNF <132681048+LennyBNF@users.noreply.github.com>
2023-06-05 08:09:45 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,61 @@
+radio.setGroup(33)
 basic.forever(function () {
     if (BitMaker.read_Din_value(GrovePort.P8) > 0) {
         basic.showLeds(`
             . . . . .
-            . # . # .
-            . . . . .
-            . # # # .
-            # . . . #
+            . . . . #
+            . . . # .
+            # . # . .
+            . # . . .
             `)
     } else {
         basic.showLeds(`
-            . . . . .
-            . # . # .
-            . . . . .
-            # . . . #
-            . # # # .
+            # . . . .
+            # . . . .
+            # . . . .
+            # . . . .
+            # . . . .
             `)
+        radio.sendString("Obstacle gauche")
+    }
+})
+basic.forever(function () {
+    if (BitMaker.read_Din_value(GrovePort.P1) > 0) {
+        basic.showLeds(`
+            . . . . .
+            . . . . #
+            . . . # .
+            # . # . .
+            . # . . .
+            `)
+    } else {
+        basic.showLeds(`
+            # # # # #
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        radio.sendString("Obstacle frontale")
+    }
+})
+basic.forever(function () {
+    if (BitMaker.read_Din_value(GrovePort.P0) > 0) {
+        basic.showLeds(`
+            . . . . .
+            . . . . #
+            . . . # .
+            # . # . .
+            . # . . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . #
+            . . . . #
+            . . . . #
+            . . . . #
+            . . . . #
+            `)
+        radio.sendString("Obstacle droite")
     }
 })
